6e5cc4963f1f02fd26584c5f38a2806c2a1153a5
Billson7 <alex.billson@sky.uk>
2021-01-10 18:44:30 +0000

    adding images to homepage image group

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,10 @@ import { client } from "../prismic-configuration";
 
 export default function Home(props) {
   const genericStyle = "h-56 w-9/12 text-center bg-gray-300 mt-2 rounded-lg ";
-  const imagePrintSrc = props.home.data.printimage?.url;
+  const imagePortraitSrc = props.homePortrait.data.portraitimage?.url;
+  const imagePrintSrc = props.homePrint.data.printimage?.url;
+  const imageGiftcardSrc = props.homeGiftcard.data.giftcardimage?.url;
+  console.log(props.homePortrait.data);
   return (
     <div className={styles.container}>
       <Head>
@@ -18,32 +21,33 @@ export default function Home(props) {
           <h1 className="text-6xl">Welcome to Art</h1>
         </div>
         <div className="mt-20 grid grid-rows-3 grid-flow-col gap-4">
-          <div className="row-span-3 box-content h-96 w-48 p-5 rounded-lg bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
+          <div
+            style={{
+              backgroundImage: `url(${imagePortraitSrc})`,
+              backgroundSize: "cover"
+            }}
+            className="row-span-3 box-content h-96 w-48 p-5 rounded-lg text-white text-center shadow-md"
+          >
             Portraits
           </div>
           <div
             style={{
               backgroundImage: `url(${imagePrintSrc})`,
-              backgroundPosition: "50%, 50%",
               backgroundSize: "cover"
             }}
-            className="col-span-2 box-content h-40 w-48 p-5 rounded-lg text-white text-center"
+            className="col-span-2 box-content h-40 w-48 p-5 rounded-lg text-white text-center shadow-md"
           >
             Prints - Coming Soon!
           </div>
-          <div className="mt-2 row-span-2 col-span-2 box-content h-40 w-48 p-5 rounded-lg bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
+          <div
+            style={{
+              backgroundImage: `url(${imageGiftcardSrc})`,
+              backgroundSize: "cover"
+            }}
+            className="mt-2 row-span-2 col-span-2 box-content h-40 w-48 p-5 rounded-lg text-center shadow-md"
+          >
             Giftcard - Coming Soon!
           </div>
-        </div>
-
-        <div id="prints" className={genericStyle}>
-          Prints - Coming Soon!
-          {/*<img*/}
-          {/*  className="w-auto overflow-hidden object-fill"*/}
-          {/*  src={imagePrintSrc}*/}
-          {/*  alt={props?.home?.alt}*/}
-          {/*/>*/}
-          {/*{console.log("DATA", props.home.data)}*/}
         </div>
 
         {/*TODO homepage*/}
@@ -59,11 +63,15 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
-  const home = await client.getSingle("home-print");
+  const homePortrait = await client.getSingle("home-portrait");
+  const homePrint = await client.getSingle("home-print");
+  const homeGiftcard = await client.getSingle("home-giftcard");
 
   return {
     props: {
-      home
+      homePortrait,
+      homePrint,
+      homeGiftcard
     }
   };
 }
